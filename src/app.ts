e9e2d611341f9cc084855e53e8ab9ad2078308c5
Ilia Pugachev <pi1691a@gmail.com>
2026-07-30 16:32:00 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,20 @@ import express from "express";
 import AuthRouter from "./routes/auth.routes.ts";
 import UserRouter from "./routes/user.routes.ts";
 import { errorHandler } from "./middlewares/error.middleware.ts";
+import cors from "cors";
 
 const app = express();
+
+const allowedOrigins = [
+    `http://localhost:${process.env.FRONTEND_PORT}`,
+    `http://10.8.1.4:${process.env.FRONTEND_PORT}`,
+];
+
+app.use(
+    cors({
+        origin: allowedOrigins,
+    }),
+);
 
 app.use(express.json());
 
